9a839c5484b530a554025a792209853f2cb417ec
Guy Zarzar <guy@seed-x.com>
2020-02-03 15:33:49 +0200

    fix reqreuiments issue

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python
 from setuptools import setup
-from fixtures_mongoengine import __version__
 
 setup(name='fixtures_mongoengine',
-      version=__version__,
+      version='1.3.1',
       description='MongoEngine Fixtures.',
       author='Vitaly Nikitin',
       author_email='alaris.nik@gmail.com',
       url='https://github.com/coderfly/fixtures-mongoengine/',
-      download_url='https://github.com/coderfly/fixtures-mongoengine/tarball/'+__version__,
-      install_requires=['mongoengine>=0.8.6', 'six'],
+      download_url='https://github.com/coderfly/fixtures-mongoengine/tarball/1.3.1',
+      install_requires=['six', 'mongoengine>=0.8.6'],
       packages=['fixtures_mongoengine'],
       keywords=['testing', 'fixtures', 'mongoengine']
       )
